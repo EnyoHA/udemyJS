6683493a8d9b3a2c3bfa3a2b339b278d911e3ccf
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-10-26 12:57:54 +0100

    hueheuhue

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/ValuesAndVariables/script.js
@@ -0,0 +1,19 @@
+let country = 'UK';
+const continent = 'europe';
+let population = '60';
+
+console.log('Values and Variables');
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log('Data Types');
+
+const isIsland = true;
+let language;
+
+console.log(isIsland);
+console.log(language);
+
+language = 'English';
--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,20 +1,36 @@
-let js = 'amazing';
-// if (js === 'amazing') alert('Javascript is fun!');
+// let js = 'amazing';
+// // if (js === 'amazing') alert('Javascript is fun!');
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-console.log("name");
-console.log(23);
+// console.log("name");
+// console.log(23);
 
-let firstName = "Enyo";
+// let firstName = "Enyo";
 
-console.log(firstName);
+// console.log(firstName);
 
-let en_hum = "eha";
-let sFunction = 27;
+// let en_hum = "eha";
+// let sFunction = 27;
 
-let person = "enyo";
-let PI = 3.115;
+// let person = "enyo";
+// let PI = 3.115;
 
-let myFirsJob = 'healthcare assisstant';
-let myCurrentJob = 'crew';
+// let myFirsJob = 'healthcare assisstant';
+// let myCurrentJob = 'crew';
+
+// console.log(true);
+// console.log(typeof true);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// javaScriptIsFun = 'Yes!';
+// console.log(javaScriptIsFun);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1996;
+
+
