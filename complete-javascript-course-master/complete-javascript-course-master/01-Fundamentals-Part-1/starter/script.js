76b34fd4bacb215cc71000c1727de22ca3c7386d
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-10-26 11:49:33 +0100

    set up

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('Javascript is fun!');
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("name");
+console.log(23);
+
+let firstName = "Enyo";
+
+console.log(firstName);
+
+let en_hum = "eha";
+let sFunction = 27;
+
+let person = "enyo";
+let PI = 3.115;
+
+let myFirsJob = 'healthcare assisstant';
+let myCurrentJob = 'crew';
